6a8f9a135afd13a0fec5d389acc4d3f04664141d
Dana Gutride <dgutride@gmail.com>
2020-06-18 09:48:21 -0400

    Add tutorial directory...

--- html-css/building-blocks/env-init.sh
+++ html-css/building-blocks/env-init.sh
@@ -1,4 +1,5 @@
 curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/site/pf-express.tar.gz > /root/pf-express.tar.gz
+mkdir /root/tutorial
 tar -xvzf /root/pf-express.tar.gz -C /root/tutorial
 cd /root/tutorial
 npm install
